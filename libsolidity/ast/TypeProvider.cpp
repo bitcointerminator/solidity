6906af53b87d6e6ae923978f3f44816895908035
Christian Parpart <christian@ethereum.org>
2019-04-01 11:42:52 +0200

    spacings for copyright header(s)

--- libsolidity/ast/TypeProvider.cpp
+++ libsolidity/ast/TypeProvider.cpp
@@ -14,6 +14,7 @@
 	You should have received a copy of the GNU General Public License
 	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include <libsolidity/ast/TypeProvider.h>
 #include <libdevcore/ArrayUtil.h>
 
